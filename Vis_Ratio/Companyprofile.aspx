e596a41204c851e25d4a6acebf79b29986d0b91a
muneeza <syeda.muniza@vis.com.pk>
2019-04-16 16:26:38 +0500

    company profile

---
+++ Vis_Ratio/Companyprofile.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/RatioForm.Master" AutoEventWireup="true" CodeBehind="Companyprofile.aspx.cs" Inherits="Vis_Ratio.Companyprofile" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
--- Vis_Ratio/Insurance.aspx.cs
+++ Vis_Ratio/Insurance.aspx.cs
@@ -12,7 +12,6 @@ namespace Vis_Ratio
 {
     public partial class Insurance : System.Web.UI.Page
     {
-
         string con = System.Configuration.ConfigurationManager.ConnectionStrings["conStr"].ToString();
 
         protected void Page_Load(object sender, EventArgs e)
@@ -39,26 +38,28 @@ namespace Vis_Ratio
             GrossPremium.Text = "";
             NetRevenue.Text = "";
             txtMyDate.Text = "";
-         }
+                }
 
         protected void Save_Click(object sender, EventArgs e)
         {
             {
+
                 SqlConnection db = new SqlConnection(con);
                 db.Open();
                 string insert = "Insert into[Vis_Ratio].[dbo].[Insurance](Adjustedequity,NetFinancialLever,OperatingLeverage,InsuranceDebt,CombinedRatio,ROAE,UnderwritingExpense,GrossClaims,NetClaims,UnderwritingProfit,GrossPremium,NetProfit,MotorPremium,ReinsuranceCeded,GrossPrem,NetPremiumRevenue,insdate) values('" + Adjustedequity.Text + "','" + NetFinancial.Text + "','" + OperatingLeverage.Text + "','" + InsurancDebt.Text + "','" + CombinedRatio.Text + "','" + ROAE.Text + "','" + UnderwritingRatio.Text + "','" + GrossClaimsRatio.Text + "','" + NetRatio.Text + "','" + Underwriting.Text + "','" + UnderwritingPremium.Text + "','" + NetProfit.Text + "','" + MotorPremium.Text + "','" + ReinsuranceCeded.Text + "','" + GrossPremium.Text + "','" + NetRevenue.Text + "','" + txtMyDate.Text + "')";
                 SqlCommand cmd = new SqlCommand(insert, db);
-                 cmd.ExecuteNonQuery();
+                cmd.ExecuteNonQuery();
                 //if (m != 0)
                 //{
-                //Response.Write("<script> alert('Data Inserted !!') </script>");
+                //Response.Write("< script > alert('Data Inserted !!') </ script >");
                 //}
                 //else
                 //{
-                //Response.Write("<script> alert('Data Inserted !!') </script >");
+                //Response.Write("< script > alert('Data Inserted !!') </ script > ");
                 //}
                 db.Close();
                 emptybox();
+
             }
         }
     }
