635da1ec5370a975a6c404587569c9c108f9b5fc
muneeza <syeda.muniza@vis.com.pk>
2019-04-17 16:47:22 +0500

    company_profile

--- Vis_Ratio/Companyprofile.aspx
+++ Vis_Ratio/Companyprofile.aspx
@@ -1,5 +1,28 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/RatioForm.Master" AutoEventWireup="true" CodeBehind="Companyprofile.aspx.cs" Inherits="Vis_Ratio.Companyprofile" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
-<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+<asp:Content ID="Companyprofile" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <link href="css/master.css" rel="stylesheet" />
+
+    <div class="center">
+        <label>Company Name</label>
+        <asp:TextBox ID="txtcompany" runat="server"></asp:TextBox>
+
+         <label>Sector Name</label>
+        <asp:DropDownList ID="Drpsector" runat="server"></asp:DropDownList>
+        <%-- <asp:TextBox ID="txtsector" runat="server"></asp:TextBox>--%>
+
+
+        <asp:Button ID="btnsave" runat="server" Text="Save" OnClick="btnsave_Click"/>
+    </div>
+
 </asp:Content>
+
+
+
+
+
+
+
+
--- Vis_Ratio/Companyprofile.aspx.cs
+++ Vis_Ratio/Companyprofile.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,8 +11,53 @@ namespace Vis_Ratio
 {
     public partial class Companyprofile : System.Web.UI.Page
     {
+        string con = System.Configuration.ConfigurationManager.ConnectionStrings["conStr"].ToString();
+
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            filldata();
+        }
+
+        private void filldata()
+        {
+            SqlConnection db = new SqlConnection(con);
+           
+            SqlCommand com = new SqlCommand("select * FROM [Vis_Ratio].[dbo].[Sectors]", db); // table name 
+            SqlDataAdapter da = new SqlDataAdapter(com);
+            DataTable dt = new DataTable();
+           
+            da.Fill(dt);  // fill dataset
+            Drpsector.DataSource = dt;
+            Drpsector.DataBind();
+            Drpsector.DataTextField = "Sector_name";
+            Drpsector.DataValueField = "Sector_id";
+            Drpsector.DataBind();
+           
+
+            //Drpsector.DataTextField = ds.Tables[0].Columns["Sector_name"].ToString(); // text field name of table dispalyed in dropdown
+            //Drpsector.DataValueField = ds.Tables[0].Columns["Sector_id"].ToString();             // to retrive specific  textfield name 
+            //Drpsector.DataSource = ds.Tables[0];      //assigning datasource to the dropdownlist
+            //Drpsector.DataBind();  //binding dropdownlist
+
+
+        }
+
+        protected void btnsave_Click(object sender, EventArgs e)
+        {
+            SqlConnection db1 = new SqlConnection(con);
+            db1.Open();
+            string insert = "Insert into[Vis_Ratio].[dbo].[Company](Company_name,Sec_code) values('" + txtcompany.Text + "','" + Drpsector.SelectedValue + "')";
+            SqlCommand cmd = new SqlCommand(insert, db1);
+            cmd.ExecuteNonQuery();
+            db1.Close();
+        }
+
+        private void emptybox()
+        {
+            txtcompany.Text = "";
+            Drpsector.SelectedIndex = 0;
+
 
         }
     }
