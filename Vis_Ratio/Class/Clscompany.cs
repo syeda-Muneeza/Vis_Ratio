214db0742adff935ddef7cfa5cf04b2bc9b07aea
muneeza <syeda.muniza@vis.com.pk>
2019-04-19 16:37:16 +0500

    add class

---
+++ Vis_Ratio/Class/Clscompany.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vis_Ratio.Class
+{
+    public class Clscompany
+    {
+
+    }
+}
--- Vis_Ratio/Corporate.aspx.cs
+++ Vis_Ratio/Corporate.aspx.cs
@@ -43,7 +43,7 @@ namespace Vis_Ratio
             FFOTermDebt.Text = "";
             Debtoverage.Text = "";
             ROAA.Text = "";
-            ROAE.Text = "";
+            ROAE.Text = ""; 
             ROAA.Text = "";
             ROAE.Text = "";
             Gearing.Text = "";
@@ -53,13 +53,12 @@ namespace Vis_Ratio
             txtMyDate.Text = "";
             Trade.Text = "";
 
-
         }
 
         protected void Save_Click(object sender, EventArgs e)
         {
-            {
-                SqlConnection db = new SqlConnection(con);
+            { 
+                SqlConnection db = new SqlConnection(con);  
                 db.Open();
                 string insert = "Insert into[Vis_Ratio].[dbo].[Corporates](FixedAssets,Longterm,StockTrade,TradeDebts,CashBank,TotalAssets,TradePayables,LongTermDebt,ShortTermDebt,TotalDebt,TotalLiabilities,TotalEquity,INCOMECASHFLOW,NetSales,GrossProfit,ProfitBeforeTax,ProfitafterTax,FFO,RATIOANALYSIS,GrossMargin,NetMargin,FFOTotalDebt,FFOtoLongDebt,ServicingCoverage,ROAA,ROAE,Gearing,Leverage,ShortTermBorrowings,CurrentRatio,cordate) values('" + FixedAssets.Text + "','" + LongDeposits.Text + "','" + Stocktrade.Text + "','" + TradeDebts.Text + "','" + CashBalances.Text + "','" + TotalAssets.Text + "','" + TradeDebts.Text + "','" + LongDebt.Text + "','" + ShortDebt.Text + "','" + TotalDebt.Text + "','" + TotalLiabilities.Text + "','" + TotalEquity.Text + "','" + NeSales.Text + "','" + GrossProfit.Text + "','" + ProfitTax.Text + "','" + ProfitAfter.Text + "','" + FFO.Text + "','" + GrossMargin.Text + "','" + NetMargin.Text + "','" + FFODebt.Text + "','" + FFOTermDebt.Text + "','" + Debtoverage.Text + "','" + ROAA.Text + "','" + ROAE.Text + "','" + ROAA.Text + "','" + ROAE.Text + "','" + Gearing.Text + "','" + Leverage.Text + "','" + Stock.Text + "','" + CurrentRatio.Text + "','" + txtMyDate.Text + "')";
                 SqlCommand cmd = new SqlCommand(insert, db);
